fc457a21080edfb054f85482980d50381564c3ec
Tofik Mohammed <tofikahm@yahoo.com>
2021-12-07 04:08:41 +0000

    implmentation of new url form submisdion

--- express_server.js
+++ express_server.js
@@ -1,7 +1,11 @@
+const PORT = 8080;
+
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -12,14 +16,26 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars ={ urls: urlDatabase}
-  res.render("urls_index",templateVars)
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
@@ -34,3 +50,43 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+function generateRandomString() {
+  let chx = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let str;
+  for (let i = 0; i < 6; i++) {
+    if (i % 2 === 0) {
+      str += chx[Math.floor(Math.random() * 25)];
+    } else {
+      str += number[Math.floor(Math.random() * 10)];
+    }
+  }
+  return str;
+}
