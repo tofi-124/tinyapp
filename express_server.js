99cfb60e5625030ae74721a9d0f8e8c2ec9ba29b
Tofik Mohammed <tofikahm@yahoo.com>
2021-12-07 11:12:07 +0000

    We'll be able to edit existing shortened URLs in our app with limplemenatation.

--- express_server.js
+++ express_server.js
@@ -39,9 +39,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id", (req, res) => {
+  delete urlDatabase[req.params.id]
+  urlDatabase[req.params.id] = req.body.newLongURL;
+  res.redirect("/urls");
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL]
-  res.redirect('/urls');
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
